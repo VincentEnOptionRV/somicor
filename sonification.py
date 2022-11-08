208f559ee05a85b2376d2ecc2328757c8479a386
VincentEnOptionRV <97223306+VincentEnOptionRV@users.noreply.github.com>
2022-11-08 19:00:14 +0100

    added more docstrings

--- sonification.py
+++ sonification.py
@@ -1,7 +1,7 @@
 import music21 as mus
 
 def generate_pitch_list():
-    """Generate the list of frequencies corresponding to notes C1 to B8 in ascending order of frequency and the list of the corresponding pitch names"""
+    """Generate the list of frequencies corresponding to notes C1 to B8 in ascending order of frequency and the list of the corresponding pitch names."""
     letters = ['c','d','e','f','g','a','b']
     pitchFreqList = [mus.pitch.Pitch(l + str(n)).frequency for l in letters for n in range(1,9)]
     pitchNameList = [l + str(n) for l in letters for n in range(1,9)]
@@ -9,19 +9,23 @@ def generate_pitch_list():
 
 
 def _asign_note(freq, pitchFreqList, pitchNameList):
-    """Find the closest musical note to the given frequency"""
+    """Find the closest musical note to the given frequency."""
     closestFreqIndex = pitchFreqList.index(min(pitchFreqList, key = lambda x: abs(x - freq)))
     return pitchNameList[closestFreqIndex]
 
 def asign_note(freq):
-    """Kind of curryfies *_asign_note*"""
+    """Kind of curryfies *_asign_note*."""
     return _asign_note(freq, *generate_pitch_list())
 
 def freqList_to_note_list(freqList):
-    """Assigns a list of notes to a list of frequencies. The notes are the ones closest to each frequency"""
+    """Assigns a list of notes to a list of frequencies. The notes are the ones closest to each frequency."""
     return list(map(asign_note, freqList))
 
 def freq_list_to_tinyNotation(freqList):
+    """
+    Convert a list of frequencies into a string representing the melody based on these frequencies in tinyNotation format.
+    The frequencies are first sorted by ascending order, so the resulting melody is also in ascending pitch.
+    """
     freqList.sort()
     noteList = freqList_to_note_list(freqList)
     lowestNote = noteList[0]
@@ -31,5 +35,6 @@ def freq_list_to_tinyNotation(freqList):
         tinyNotation += " " + note[0] + "'" * nbOfOctavesUp + "4"
     return tinyNotation
 
-def sonification(freqList):    
+def sonification(freqList):
+    """Performs the sonification : convert a list of frequencies into a music21 Stream."""
     return mus.converter.parse(freq_list_to_tinyNotation(freqList))
